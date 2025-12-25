a0709ba0ceb5beafea59482be8a018eb0b1ddce1
JanviArora24 <janvi.jiyaarora@gmail.com>
2025-12-25 14:00:28 +0530

    fix: use backend env url in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
